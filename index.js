c9bcd2f394e0965e492541f56282608a32366224
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-29 15:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   var rankedList = []
   rankedList = document.querySelectorAll('ul.ranked-list li')
   for (let i=0; i<rankedList.length;i++) {
-    var value = rankedList[i].innerHTML
+    var value = parseInt(rankedList[i].innerHTML)
     value += 1;
     rankedList[i].innerHTML = value
   }
