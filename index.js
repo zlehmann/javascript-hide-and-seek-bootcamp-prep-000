fd47fde5e71fdb4b4cbb0c21383f63a85a5a9947
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-29 15:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+  var rankedList = []
   rankedList = document.querySelectorAll('ul.ranked-list li')
   for (let i=0; i<rankedList.length;i++) {
     rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML + 1)
