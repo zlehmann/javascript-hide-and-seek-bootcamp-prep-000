52f5ff59517189682d6bf8c96f2318a6824e1d86
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-29 14:55:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector()
+  return document.getElementById('nested').querySelector(.target)
 }
